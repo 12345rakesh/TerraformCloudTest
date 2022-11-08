66ae38a94922afaf8ae78859788212f46db07cf9
12345rakesh <rakesh@gmail.com>
2022-11-08 20:14:27 +0000

    fff

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
+
 resource "aws_instance" "instance1" {
 ami = "ami-06640050dc3f556bb"
 instance_type = "t2.micro"
 key_name = "jenkins"
-security_groups = [ "sg-0849cff32e5900ed6" ]
 
 tags = {
  Name = "redhat-8"
--- main.tf
+++ main.tf
@@ -16,3 +16,14 @@ provider "aws"  {
 
 
 }
+
+resource "aws_instance" "instance1" {
+ami = "ami-06640050dc3f556bb"
+instance_type = "t2.micro"
+key_name = "jenkins"
+
+tags = {
+ Name = "redhat-8"
+
+ }
+}
